3e927ad100ed407d50f7e2fc1cd8182ce49d3a89
Jean Trujillo <91339890+jeantpdev@users.noreply.github.com>
2024-04-01 23:39:32 -0500

    pagina base

---
+++ src/components/menu/Navigation.jsx
@@ -0,0 +1,91 @@
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'Inicio', href: '/', current: true },
+  { name: 'Prueba', href: '/prueba/', current: false },
+  { name: 'Acerca de', href: '/acerca-de/', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Navigation() {
+  return (
+    <Disclosure as="nav">
+      {({ open }) => (
+        <>
+          <div className="mx-auto px-14 xl:px-64 sm:py-2 bg-gray-800">
+            <div className="relative flex h-16 items-center justify-start">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <img
+                    className="h-10 w-auto"
+                    src="/img/breast-cancer-img.png"
+                    alt="Your Company"
+                  />
+                </div>
+
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>       
+
+              </div>
+              <div className="hidden sm:block sm:justify-end">
+                <a href="/prueba/" className=" font-medium px-5 py-2.5 rounded text-center transition focus-visible:ring-2 ring-offset-2 ring-gray-200 bg-pink-500 text-white hover:bg-pink-600 border-2 border-transparent">Realizar prueba</a>
+                </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-400 hover:bg-gray-500 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
+
